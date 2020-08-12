51c2c2e887909c61171c63c83b5feba5659397ce
ReedS <ReedStock1992@gmail.com>
2020-08-11 22:17:53 -0700

    Contact me form working

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -14,12 +14,16 @@ export function ContactMe(props: IProps | undefined) {
             <Grid container justify="center">
                 <Grid item xl={4} lg={4} md={6} sm={8} xs={10}>
                     <Paper style={{ paddingTop: "4em", paddingBottom: "4em", paddingLeft: "4em", paddingRight: "4em" }}>
-                        <Form>
+                        <Form action="https://formspree.io/mvowyqdw" method="POST">
                             <Form.Field
+                                type="text" 
+                                name="name"
                                 control={Input}
                                 placeholder="Name"
                             />
                             <Form.Field
+                                type="email" 
+                                name="_replyto"
                                 control={Input}
                                 placeholder="Email"
                             />
@@ -32,6 +36,7 @@ export function ContactMe(props: IProps | undefined) {
                                 style={{ backgroundColor: "#fdd835" }}
                                 control={Button}
                                 content="Send"
+                                type="submit"
                             />
                         </Form>
                     </Paper>
